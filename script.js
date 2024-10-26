96a1b83162e364a3929b56962f7a32799a571ae5
Behruz <Behruz>
2024-10-26 18:00:40 +0300

    IZANA

--- script.js
+++ script.js
@@ -13,21 +13,34 @@ function rec(i = 0) {
 }
 rec();
 
+// `projects_text` elementini topamiz
+const projectText = document.querySelector('.projects_text');
+const projectTextContent = projectText.innerHTML;
+projectText.innerHTML = '';
+
+function animateProjectText(i = 0) {
+  projectText.innerHTML += projectTextContent[i];
+  i++;
+  if (i < projectTextContent.length) {
+    setTimeout(() => {
+      animateProjectText(i);
+    }, 50);
+  }
+}
+animateProjectText();
+
 const nav_link = document.querySelector('.zone_list');
 const virtual_join = document.querySelector('.virtual_join');
 const virtual_watch = document.querySelector('.virtual_watch');
-
 const virtual_img = document.querySelector('.virtual_img');
 
 function animation() {
-  virtual_img.style.transform = `translateX(${scrollY})`
-  nav_link.style.transform = `translateX(${scrollY})`
-  virtual_join.style.transform = `translateX(${scrollY})`
-  virtual_watch.style.transform = `translateX(${scrollY})`
+  virtual_img.style.transform = `translateX(${scrollY}px)`;
+  nav_link.style.transform = `translateX(${scrollY}px)`;
+  virtual_join.style.transform = `translateX(${scrollY}px)`;
+  virtual_watch.style.transform = `translateX(${scrollY}px)`;
   console.log(scrollY);
 }
 
-
 window.addEventListener('scroll', animation);
 window.addEventListener('load', animation);
-
